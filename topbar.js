3b2cb72df06d323a87ef68fecfc90380eb0a6fc0
Timothy Erbert <timjsherbert@gmail.com>
2024-06-19 01:47:42 -0500

    Create topbar.js

---
+++ topbar.js
@@ -0,0 +1,16 @@
+function tbDropToggle() {
+  document.getElementById("tbDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.tb-hamburger-button')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
